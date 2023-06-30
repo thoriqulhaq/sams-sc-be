dc45ffff156bd2d57667f333179a21d668144baf
NostrumPyre <watchdogs13.xv@gmail.com>
2023-06-30 16:25:06 +0800

    add getStudentData to route

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { createStudent } = require('../controller/studentController')
+const { getStudentData } = require('../controller/studentController')
 
 router.route('/').post(createStudent)
 router.route('/').get(getStudentData)
