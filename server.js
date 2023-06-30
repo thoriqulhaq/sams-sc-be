0233b2a536ee2656fb40ddbfe2a1a2d6de635207
Thoriqulhaq Jibril <thoriqulhaqjibril1@gmail.com>
2023-06-30 19:16:24 +0800

    Implement CORS

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 4000
 
 connectDB()
 
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
